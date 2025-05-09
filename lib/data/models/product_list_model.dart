17fa85061db0eae039672ab20154f1d3ffdc94d7
Md Alhaz Mondal Hredhay <amhredhay102@gmail.com>
2025-05-09 17:16:50 +0600

    Products list screen api integration in progress

---
+++ lib/data/models/product_list_model.dart
@@ -0,0 +1,159 @@
+class ProductListsModel {
+  int? currentPage;
+  List<ProductData>? data;
+  String? firstPageUrl;
+  int? from;
+  int? lastPage;
+  String? lastPageUrl;
+  List<Links>? links;
+  String? nextPageUrl;
+  String? path;
+  int? perPage;
+  String? prevPageUrl;
+  int? to;
+  int? total;
+
+  ProductListsModel({
+    this.currentPage,
+    this.data,
+    this.firstPageUrl,
+    this.from,
+    this.lastPage,
+    this.lastPageUrl,
+    this.links,
+    this.nextPageUrl,
+    this.path,
+    this.perPage,
+    this.prevPageUrl,
+    this.to,
+    this.total,
+  });
+
+  ProductListsModel.fromJson(Map<String, dynamic> json) {
+    currentPage = json['current_page'];
+    if (json['data'] != null) {
+      data = <ProductData>[];
+      json['data'].forEach((v) {
+        data!.add(ProductData.fromJson(v));
+      });
+    }
+    firstPageUrl = json['first_page_url'];
+    from = json['from'];
+    lastPage = json['last_page'];
+    lastPageUrl = json['last_page_url'];
+    if (json['links'] != null) {
+      links = <Links>[];
+      json['links'].forEach((v) {
+        links!.add(Links.fromJson(v));
+      });
+    }
+    nextPageUrl = json['next_page_url'];
+    path = json['path'];
+    perPage = json['per_page'];
+    prevPageUrl = json['prev_page_url'];
+    to = json['to'];
+    total = json['total'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['current_page'] = currentPage;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    data['first_page_url'] = firstPageUrl;
+    data['from'] = from;
+    data['last_page'] = lastPage;
+    data['last_page_url'] = lastPageUrl;
+    if (links != null) {
+      data['links'] = links!.map((v) => v.toJson()).toList();
+    }
+    data['next_page_url'] = nextPageUrl;
+    data['path'] = path;
+    data['per_page'] = perPage;
+    data['prev_page_url'] = prevPageUrl;
+    data['to'] = to;
+    data['total'] = total;
+    return data;
+  }
+}
+
+class ProductData {
+  int? id;
+  String? name;
+  String? buyingPrice;
+  String? sellingPrice;
+  int? stock;
+  String? img;
+  String? brandName;
+  String? categoryName;
+  String? status;
+  String? createdAt;
+  String? updatedAt;
+
+  ProductData({
+    this.id,
+    this.name,
+    this.buyingPrice,
+    this.sellingPrice,
+    this.stock,
+    this.img,
+    this.brandName,
+    this.categoryName,
+    this.status,
+    this.createdAt,
+    this.updatedAt,
+  });
+
+  ProductData.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    name = json['name'];
+    buyingPrice = json['buying_price'];
+    sellingPrice = json['selling_price'];
+    stock = json['stock'];
+    img = json['img'];
+    brandName = json['brand_name'];
+    categoryName = json['category_name'];
+    status = json['status'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    data['name'] = name;
+    data['buying_price'] = buyingPrice;
+    data['selling_price'] = sellingPrice;
+    data['stock'] = stock;
+    data['img'] = img;
+    data['brand_name'] = brandName;
+    data['category_name'] = categoryName;
+    data['status'] = status;
+    data['created_at'] = createdAt;
+    data['updated_at'] = updatedAt;
+    return data;
+  }
+}
+
+class Links {
+  String? url;
+  String? label;
+  bool? active;
+
+  Links({this.url, this.label, this.active});
+
+  Links.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+    label = json['label'];
+    active = json['active'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['url'] = url;
+    data['label'] = label;
+    data['active'] = active;
+    return data;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
+import 'core/di/dependency_injection_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await diSetup();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]).then((_) => runApp(const ProductHuntApp()));
